1b298154ad578fc7c0eb278c2bec8602dc35d840
zach <yzpag@gmail.com>
2025-04-20 21:12:43 +0800

    finsh 1week assignment

--- week1/part-3-hangman/src/main.rs
+++ week1/part-3-hangman/src/main.rs
@@ -37,4 +37,57 @@ fn main() {
     // println!("random word: {}", secret_word);
 
     // Your code here! :)
+
+    let len = secret_word.len();
+    let mut guessted_result: Vec<char> = std::iter::repeat('-').take(len).collect();
+    let mut guessted_letters: Vec<char> = vec![];
+    let mut try_times = NUM_INCORRECT_GUESSES;
+
+    println!("Welcome to CS110L Hangman!");
+    while try_times > 0 && guessted_result != secret_word_chars {
+        println!(
+            "The word so far is {}",
+            guessted_result.iter().collect::<String>()
+        );
+        println!(
+            "You have guessed the following letters:{}",
+            guessted_letters.iter().collect::<String>()
+        );
+        println!("You have {} guesses left", try_times);
+
+        print!("Please guess a letter: ");
+        // Make sure the prompt from the previous line gets displayed:
+        io::stdout().flush().expect("Error flushing stdout.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error reading line.");
+        let guess_vec: Vec<char> = guess.chars().collect();
+        // WARN: 直接获取，忽略报错
+        let guess_ch = guess_vec.get(0).unwrap();
+        guessted_letters.push(*guess_ch);
+        let mut flag = false;
+
+        for (i, ch) in secret_word_chars.iter().enumerate() {
+            if ch == guess_ch {
+                guessted_result[i] = *ch;
+                flag = true;
+                try_times = NUM_INCORRECT_GUESSES;
+            }
+        }
+        if !flag {
+            try_times -= 1;
+            println!("Sorry, that letter is not in the word");
+        }
+        println!("");
+    }
+
+    if guessted_result == secret_word_chars {
+        println!(
+            "Congratulations you guessed the secret word: {}!",
+            guessted_result.iter().collect::<String>()
+        );
+    } else {
+        println!("Sorry, you ran out of guesses!");
+    }
 }
